3d217901c21dc92f2ced47d7bd99bb6dec923fb6
Jerome Cornet <jerome@jeromecornet.com>
2021-04-01 11:05:38 -0400

    With new option

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :room
-  broadcasts_to :room, inserts_by: :append, replace_if_present: :itself
+  broadcasts_to :room, inserts_by: :append, remove_if_present: :itself
 end
